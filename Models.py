af1eb32bf8232e2c6a38af4710ae3da91da95daf
ErikHys <E.Hystad@student.uib.no>
2021-11-08 15:01:31 +0100

    added models file to get models

--- Models.py
+++ Models.py
@@ -1,4 +1,5 @@
 from simple_elmo import ElmoModel
+from transformers import pipeline
 
 
 def get_nor_elmo(path="Vectors/218.zip"):
@@ -6,5 +7,10 @@ def get_nor_elmo(path="Vectors/218.zip"):
     nor_elmo.load(path)
     return nor_elmo
 
-def get_nor_bert():
-    pass
+
+def get_nor_bert(tokenizer="sentiment-analysis"):
+    return pipeline(tokenizer, model='ltgoslo/norbert')
+
+
+def get_nb_bert(tokenizer="sentiment-analysis"):
+    return pipeline(tokenizer, model='NbAiLab/nb-bert-base-mnli')
