3701e5c81ff16093c3081efd9c5b5b003b01181e
ErikHys <E.Hystad@student.uib.no>
2021-11-09 21:01:52 +0100

    small abstraction, as ugly as bendik

--- Models.py
+++ Models.py
@@ -8,17 +8,19 @@ def get_nor_elmo(path="Vectors/218.zip"):
     return nor_elmo
 
 
-def get_nor_bert(task="sentiment-analysis"):
+def get_nor_bert(num_labels, task="sentiment-analysis", model_type=BertForTokenClassification):
     tokenizer = AutoTokenizer.from_pretrained("ltgoslo/norbert")
-    return pipeline(task, tokenizer=tokenizer, model='ltgoslo/norbert')
+    model = model_type.from_pretrained("ltgoslo/norbert", num_labels=num_labels)
+    return pipeline(task, tokenizer=tokenizer, model=model)
 
 
-def get_nb_bert(task="sentiment-analysis"):
+def get_nb_bert(num_labels, task="sentiment-analysis", model_type=BertForTokenClassification):
     tokenizer = AutoTokenizer.from_pretrained('NbAiLab/nb-bert-base-mnli')
-    return pipeline(task, tokenizer=tokenizer, model='NbAiLab/nb-bert-base-mnli')
+    model = model_type.from_pretrained('NbAiLab/nb-bert-base-mnli', num_labels=num_labels)
+    return pipeline(task, tokenizer=tokenizer, model=model)
 
 
-def get_mbert(num_labels, task="sentiment-analysis"):
+def get_mbert(num_labels, task="sentiment-analysis", model_type=BertForTokenClassification):
     tokenizer = AutoTokenizer.from_pretrained('bert-base-multilingual-cased')
-    model = BertForTokenClassification.from_pretrained('bert-base-multilingual-cased', num_labels=num_labels)
+    model = model_type.from_pretrained('bert-base-multilingual-cased', num_labels=num_labels)
     return pipeline(task, tokenizer=tokenizer, model=model)
