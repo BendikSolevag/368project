38685b6056dffdd9bcb8e591b0067bb205aa4d06
ErikHys <E.Hystad@student.uib.no>
2021-11-09 20:43:35 +0100

    Working mBert

--- Models.py
+++ Models.py
@@ -1,5 +1,5 @@
 from simple_elmo import ElmoModel
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, BertForTokenClassification
 
 
 def get_nor_elmo(path="Vectors/218.zip"):
@@ -8,13 +8,17 @@ def get_nor_elmo(path="Vectors/218.zip"):
     return nor_elmo
 
 
-def get_nor_bert(tokenizer="sentiment-analysis"):
-    return pipeline(tokenizer, model='ltgoslo/norbert')
+def get_nor_bert(task="sentiment-analysis"):
+    tokenizer = AutoTokenizer.from_pretrained("ltgoslo/norbert")
+    return pipeline(task, tokenizer=tokenizer, model='ltgoslo/norbert')
 
 
-def get_nb_bert(tokenizer="sentiment-analysis"):
-    return pipeline(tokenizer, model='NbAiLab/nb-bert-base-mnli')
+def get_nb_bert(task="sentiment-analysis"):
+    tokenizer = AutoTokenizer.from_pretrained('NbAiLab/nb-bert-base-mnli')
+    return pipeline(task, tokenizer=tokenizer, model='NbAiLab/nb-bert-base-mnli')
 
 
-def get_mbert(tokenizer="sentiment-analysis"):
-    return pipeline(tokenizer, model='bert-base-multilingual-cased')
+def get_mbert(num_labels, task="sentiment-analysis"):
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-multilingual-cased')
+    model = BertForTokenClassification.from_pretrained('bert-base-multilingual-cased', num_labels=num_labels)
+    return pipeline(task, tokenizer=tokenizer, model=model)
