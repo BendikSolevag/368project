51f42aa89adac1223fac2e6569adb1aa6fb96db0
Baynrich <bendikasolevaag@gmail.com>
2021-11-10 13:34:45 +0100

    Refactor sentence level sentiment polarity to make more sense

--- sentence_level_sentiment_polarity.py
+++ sentence_level_sentiment_polarity.py
@@ -1,6 +1,6 @@
 """ Fetch Models """
 import torch
-import Models
+from ..Models import Models
 from transformers import BertForSequenceClassification
 
 
@@ -17,18 +17,18 @@ def number_sentiment(sentiment):
         return 1
     return 0
 
-with open('./sentiment_polarity/train.json') as polarity_data:
-    polarity_array = json.load(polarity_data)[:8]
+with open('./sentence_level_sentiment_polarity/train.json') as polarity_data:
+    polarity_array = json.load(polarity_data)[:5]
     train_texts = [datapoint['text'] for datapoint in polarity_array]
     train_labels = [number_sentiment(datapoint['label']) for datapoint in polarity_array]
     
-with open('./sentiment_polarity/val.json') as polarity_data:
-    polarity_array = json.load(polarity_data)[:8]
+with open('./sentence_level_sentiment_polarity/val.json') as polarity_data:
+    polarity_array = json.load(polarity_data)[:5]
     val_texts = [datapoint['text'] for datapoint in polarity_array]
     val_labels = [number_sentiment(datapoint['label']) for datapoint in polarity_array]
     
-with open('./sentiment_polarity/test.json') as polarity_data:
-    polarity_array = json.load(polarity_data)[:8]
+with open('./sentence_level_sentiment_polarity/test.json') as polarity_data:
+    polarity_array = json.load(polarity_data)[:5]
     test_texts = [datapoint['text'] for datapoint in polarity_array]
     test_labels = [number_sentiment(datapoint['label']) for datapoint in polarity_array]
 
@@ -131,13 +131,11 @@ def eval(model, dataset):
                 FP += 1
     return f1_score(TP, FP, FN), accuracy(TP, TN, FP, FN)
 
-
 nor_bert_model=nor_bert_pipe.model
 nor_bert_model.to(device)
 nor_bert_optim = AdamW(nor_bert_model.parameters(), lr=5e-5)
 tune(nor_bert_model, nor_bert_optim, nor_bert_train_dataset)
 nor_bert_f1, nor_bert_accuracy = eval(nor_bert_model, nor_bert_test_dataset)
-
 
 nb_bert_model = nb_bert_pipe.model
 nb_bert_model.to(device)
@@ -145,11 +143,13 @@ nb_bert_optim = AdamW(nb_bert_model.parameters(), lr=5e-5)
 tune(nb_bert_model, nb_bert_optim, nb_bert_train_dataset)
 nb_bert_f1, nb_bert_accuracy = eval(nb_bert_model, nb_bert_test_dataset)
 
+
 mbert_model = mbert_pipe.model
 mbert_model.to(device)
 mbert_optim = AdamW(mbert_model.parameters(), lr=5e-5)
 tune(mbert_model, mbert_optim, mbert_train_dataset)
 mbert_f1, mbert_accuracy = eval(mbert_model, mbert_test_dataset)
+
 
 print('NorBert - F1 score: ', nor_bert_f1, ' Accuracy: ', nor_bert_accuracy)
 print('NbBert - F1 score: ', nb_bert_f1, ' Accuracy: ', nb_bert_accuracy)
